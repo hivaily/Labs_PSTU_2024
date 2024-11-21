b261015a574dc620137a11eff72d5ca6c02072e1
hiva <ilyashramchov05@gmail.com>
2024-11-21 16:12:41 +0500

    Update 5_pointer.cpp

--- Sem_1/5_pointer/5_pointer.cpp
+++ Sem_1/5_pointer/5_pointer.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 int main()
 {
-	system("chcp 1251>NULL");
-
 	int n, fact = 1, * f = &fact;
 
-	cout << ("Введите нужное число "); cin >> n;
+	cout << ("Input N "); cin >> n;
 
-	for (int i = 1; i <= n; i++) {*f *= i;}
-	
-	cout << ("Факториал ") << n << " равен " << *f;
+	for (int i = 1; i <= n; i++) { *f *= i; }
+
+	cout << ("Fact of  ") << n << " = " << *f;
 
 	return 0;
 }
