718d351622c175f70254b292e637222cb04ee5b6
hiva <ilyashramchov05@gmail.com>
2024-11-21 16:02:28 +0500

    Update 1_switch.cpp

--- Sem_1/1_switch/1_switch.cpp
+++ Sem_1/1_switch/1_switch.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main()
 {
-	system("chcp 1251>NULL");
 	float x, y;
 	int z;
 	cin >> x >> y >> z;
@@ -12,7 +11,7 @@ int main()
 
 	if (y == 0 and z == 4)
 	{
-		cout << "ƒелить на ноль нельз€!"; return 0;
+		cout << "Cant divide by 0!"; return 0;
 	}
 
 	else
@@ -24,7 +23,7 @@ int main()
 		case 2: cout << x - y; break;
 		case 3: cout << x * y; break;
 		case 4: cout << x / y; break;
-		default: cout << "¬веден неправильный код операции";
+		default: cout << "Wrong operation code";
 		}
 
 		return 0;
