f08cd5f540c41ebdc98979f6ab78e09c8d9742b1
hiva <ilyashramchov05@gmail.com>
2024-11-21 16:42:42 +0500

    Add files via upload

---
+++ Sem_1/2_8/2_8.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	system("chcp 1251>NULL");
+	int x, y, n;
+
+
+	cout << "Input N "; cin >> x;
+
+	if (x <= 2) { cout << "Cant build triangle with that N"; return 0; }
+	y = x - 1;
+	n = x;
+	x = 1;
+	for (int i = 1; i < n + 1; i++)
+	{
+		cout << string(x, '*') << string(y, ' ') << endl;
+		y -= 1;
+		x += 1;
+
+
+	}
+
+	return 0;
+
+
+
+}
