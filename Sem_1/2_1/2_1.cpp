bab4b3a7edc79b4240471aee7325d288fb0adaab
hiva <ilyashramchov05@gmail.com>
2024-11-21 15:46:55 +0500

    Update 2_1.cpp

--- Sem_1/2_1/2_1.cpp
+++ Sem_1/2_1/2_1.cpp
@@ -3,19 +3,21 @@
 using namespace std;
 int main()
 {
-	system("chcp 1251>NULL");
 	int x, y, z;
 	y = 0;
-	cout << "¬ведите количество натуральных чисел "; cin >> x;
+	cout << "Input N "; cin >> x;
+	if (x <= 0) { cout << "Not natural number!"; return 0; }
+
 	z = x;
-	for (int i = 1; i < z+1; i++)
+	for (int i = 1; i < z + 1; i++)
 	{
 		y += x;
 		x -= 1;
 	}
 
-	cout << "—умма этого количества первых натуральных чисел равна " << y;
+	cout << "Sum =  " << y;
 	return 0;
+
 
 
 
