0f972453f835d2d7f05332ba58ce700bf8a64a64
hiva <ilyashramchov05@gmail.com>
2024-11-21 16:09:02 +0500

    Update 4_pointer.cpp

--- Sem_1/4_pointer/4_pointer.cpp
+++ Sem_1/4_pointer/4_pointer.cpp
@@ -8,17 +8,17 @@ int main()
 	system("chcp 1251>NULL");
 	int n, tmp, * ptr = &tmp, max, min;
 
-	cout << "Введите длину последовательности и первый её член "; cin >> n >> *ptr;
+	cout << "Input number of nubmers in sequence and its first member "; cin >> n >> *ptr;
 	max = *ptr;
 	min = *ptr;
 
 	for (int i = 1; i < n; i++)
 	{
-		cout << "Введите член последовательности "; cin >> *ptr;
+		cout << "Input member of sequence "; cin >> *ptr;
 		if (*ptr > max) { max = *ptr; }
 		else if (*ptr < min) { min = *ptr; }
 	}
-	cout << "Максимум = " << max << endl << "Минимум = " << min;
+	cout << "Max = " << max << endl << "Min = " << min;
 
 
 
