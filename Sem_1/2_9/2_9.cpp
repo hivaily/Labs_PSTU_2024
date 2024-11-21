ce98ae4db06006db830e6dd509175181f7a422ae
hiva <ilyashramchov05@gmail.com>
2024-11-21 17:25:04 +0500

    Add files via upload

---
+++ Sem_1/2_9/2_9.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int x, y, n, b;
+
+	cout << "Input  N "; cin >> x;
+	y = x;
+	n = (x - 2)*2+1;
+	b = 0;
+
+	cout << string(y, ' ');
+
+	for (int i = 0; i < x; i++)
+	{
+		cout << '*' << ' ';
+	}
+
+	cout << endl;
+	y--;
+
+	for (int i = 1; i < x-1; i++)
+	{
+		cout << string(y, ' ') << '*' << string(n,' ') << '*' << string(b,' ') << '*' << endl;
+		y -= 1;
+		b += 1;
+	}
+
+
+	cout << string(y, ' ');
+
+	for (int i = 0; i < x; i++)
+	{
+		cout << '*' << ' ';
+	}
+
+	b-= 1;
+	cout << string(b, ' ') << '*' << endl;
+
+	
+
+
+	for (int i = 1; i < x - 1; i++)
+	{
+		cout << string(y, ' ') << '*' << string(n, ' ') << '*' << string(b,' ') << '*' <<  endl;
+		b -= 1;
+	}
+
+	cout << string(y, ' ');
+
+	for (int i = 0; i < x; i++)
+	{
+		cout << '*' << ' ';
+	}
+
+	cout << endl;
+}
