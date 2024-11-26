100dca111ed6b99e968021b0ee2e6d7d2b1b8f50
hiva <ilyashramchov05@gmail.com>
2024-11-26 16:26:58 +0500

    Add files via upload

---
+++ Sem_1/2_13/2_13.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float a, b, c, D, x;
+	int M;
+
+	cout << "Input coefficients a, b, c "; cin >> a >> b >> c;
+
+	D = pow(b,2) - 4 * a * c;
+
+	if (D < 0) { M = 1; }
+	else if (D > 0) { M = 2; }
+	else { M = 3; }
+
+	switch (M)
+	{
+	case 1: cout << "No solution"; break;
+	case 2: cout << "Two solutions : " << (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a) << " and " << (-b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a); break;
+	case 3: cout << "One solution : " << -b / (2 * a); break;
+	}
+
+	return 0;
+}
