1cc3c0087785ccd3e13ca25d5a1c431d9a6de8cf
hiva <ilyashramchov05@gmail.com>
2024-11-26 16:26:36 +0500

    Add files via upload

---
+++ Sem_1/2_12/2_12.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, tmp, max, min;
+
+	cout << "Input number of nubmers in the sequence "; cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "Input positive member of sequence "; cin >> tmp;
+		if (tmp <= 0) { cout << "Not positive number!" << endl; i -= 1; continue; }
+		else if (i == 1) { max = tmp; min = tmp; }
+		else if (tmp > max) { max = tmp; }
+		else if (tmp < min) { min = tmp; }
+	}
+	cout << "Sum of max and min = " << max + min;
+
+	return 0;
+}
