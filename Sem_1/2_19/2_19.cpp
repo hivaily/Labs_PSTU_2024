1b7a0e974a6f3d3d93cdb6ccf964dca6381015e8
hiva <ilyashramchov05@gmail.com>
2024-11-28 17:13:05 +0500

    Add files via upload

---
+++ Sem_1/2_19/2_19.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a, sum = 0;
+	cout << "Input N "; cin >> a;
+	if (a <= 0) { cout << "Error. N cant be <= than 0"; return 0; }
+
+	for (int i = 1; i <= a; i++)
+	{
+		if (i % 3 == 0) { sum -= i; }
+		else { sum += i; }
+	}
+
+	cout << "Sum = " << sum;
+	return 0;
+
+}
