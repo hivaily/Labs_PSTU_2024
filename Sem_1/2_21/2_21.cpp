9dec68ec84ecb53516c4b245475864b5076b2260
hiva <ilyashramchov05@gmail.com>
2024-11-28 17:29:17 +0500

    Add files via upload

---
+++ Sem_1/2_21/2_21.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a = 1, b , c;
+	bool F = false;
+
+	for (int i = 1; i <= 3; i)
+	{
+		cout << "Input member of the sequence, input 0 to exit "; cin >> c;
+		if (i == 1) { b = c; i++; continue; }
+		if (c == 0) { break; }
+		if (c <= b) { F = true; }
+		b = c;
+
+	}
+
+	if (F == true) { cout << "Not increasing"; }
+	else { cout << "Increasing"; }
+
+	return 0;
+
+}
