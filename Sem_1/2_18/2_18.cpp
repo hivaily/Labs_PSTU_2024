a7169065305680df044ba0d898e4672bcf6799d8
hiva <ilyashramchov05@gmail.com>
2024-11-28 17:09:48 +0500

    Add files via upload

---
+++ Sem_1/2_18/2_18.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a, sum = 0;
+	cout << "Input N "; cin >> a;
+	if (a <= 0) { cout << "Error. N not natural number!"; return 0; }
+	
+	cout << "Reversed N: ";
+
+	while (a != 0)
+	{	
+		cout << a % 10;
+		a /= 10;
+	}
+
+	return 0;
+}
