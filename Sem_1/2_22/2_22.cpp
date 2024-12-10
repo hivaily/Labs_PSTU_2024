e9008184abf2e25a7cd07bdd59902b8ef5e493f6
hiva <ilyashramchov05@gmail.com>
2024-12-10 15:48:01 +0500

    Add files via upload

---
+++ Sem_1/2_22/2_22.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	float n, s, a;
+	int i = 1;
+	bool flag = false;
+	cin >> n >> s;
+	while (i <= n && !flag)
+	{
+		a = sin(n + i / n);
+		if (a == s) { flag = true; }
+		else { i++; }
+	}
+	if (flag)
+	{
+		cout << "N Found!" << endl;
+	}
+	else
+	{
+		cout << "N Not found!" << endl;
+	}
+		return 0;
+}
