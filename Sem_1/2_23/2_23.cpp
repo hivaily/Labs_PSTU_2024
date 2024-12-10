85a549d4ef6c27163cca553b59790681f4175b2c
hiva <ilyashramchov05@gmail.com>
2024-12-10 16:02:10 +0500

    Add files via upload

---
+++ Sem_1/2_23/2_23.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	float n, a, max;
+	int i = 2;
+	int count = 1;
+
+	cin >> n;
+
+	max = sin(n + 1 / n);
+
+	while (i <= n)
+	{
+		a = sin(n + i / n);
+		if (a > max)
+		{
+			max = a;
+			count = 1;
+		}
+		else if (a == max) { count++; }
+		i++;
+	}
+	cout << "Максимальный элемент: " << max << endl;
+	cout << "Число элементов с этим значением: " << count << endl;
+
+	return 0;
+}
