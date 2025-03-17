32c9571d55a6289f9892f7d9520ef7f7633f50e8
hiva <ilyashramchov05@gmail.com>
2025-03-17 10:17:13 +0500

    Add files via upload

---
+++ Sem_2/Lab_5.1/Lab_5.1.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+using namespace std;
+/* var 24.
+ а) для массива целых чисел удаляет все четные элементы из
+ массива;
+ б) для строки удаляет все четные слова
+ */
+
+int* sol(int n, int mas[])
+{
+	int* nmas = new int[n];
+	int tmp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (mas[i] % 2 != 0) { nmas[tmp] = mas[i]; tmp++; }
+	}
+	for (int i = 0; i < tmp; i++)
+	{
+		cout << nmas[i] << ' ';
+
+	}
+
+	return 0;
+
+
+}
+
+
+string sol(int n, string str)
+{
+	int len = str.length();
+	string* nmas = new string[n];
+	string tmpword = "";
+	int tmp = 0;
+	for (int i = 0; i < len; i++)
+	{
+		if (str[i] != ' ') { tmpword = tmpword + str[i]; }
+		else { nmas[tmp] = tmpword; tmpword = ""; tmp++; }
+	}
+	nmas[tmp] = tmpword;
+	tmp = 0;
+	string* rmas = new string[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		if ((i + 1) % 2 != 0) { rmas[tmp] = nmas[i]; tmp++; }
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << rmas[i] << ' ';
+	}
+
+	return "0";
+}
+
+
+
+
+int main()
+{
+	int mas[8] = { 5,8,1,2,10,25,3,9 };
+	string str = "cha maganera mattagan pooreh ganchan round and round";
+	int n = 8;
+
+	cout << "Original int array : ";
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << mas[i] << ' ';
+	}
+
+	cout << "\nOriginal string : " << str << "\n\n";
+	cout << "Changed int array : ";
+	sol(n, mas);
+	cout << "\nChanged string : ";
+	sol(n, str);
+	cout << "\n";
+
+	return 0;
+}
---
+++ Sem_2/Lab_5.2/Lab_5.2.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <cstdarg>
+using namespace std;
+/*Написать функцию (или макроопределение), которая
+находит длину стороны по координатам его точек..
+Написать функцию square, которая вычисляет площадь
+треугольника, заданного координатами вершин. Написать
+функцию  squaren c переменным числом параметров,
+которая определяет площадь выпуклого многоугольника,
+заданного координатами своих вершин.
+ */
+
+
+
+long double len(int x1, int y1, int x2, int y2)
+{
+	long double side = 0;
+	side = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+	return side;
+}
+
+long double square(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+	long double area = 0;
+	area = 0.5 * ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3));
+	return abs(area);
+}
+
+double squaren(int k, ...)
+{
+	double area = 0;
+	double x1, y1, fx1, fy1;
+	va_list poly;
+	va_start(poly, k);
+	int n = k / 2;
+	x1 = va_arg(poly, int);
+	y1 = va_arg(poly, int);
+	fx1 = x1;
+	fy1 = y1;
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		int x2 = va_arg(poly, int);
+		int y2 = va_arg(poly, int);
+		area += (x1 * y2 - x2 * y1);
+		x1 = x2;
+		y1 = y2;
+
+
+	}
+
+	area += (x1 * fy1 - fx1 * y1);
+	area /= 2;
+
+	return abs(area);
+}
+
+
+
+void main()
+{
+	cout << "\nLength of side with coordinates (2,5) , (3,2) = " << len(2, 5, 3, 2);
+	cout << "\nArea of a triangle with coordinates (5,7) , (0,2) , (-3,4) = " << square(5, 7, 0, 2, -3, 4);
+	cout << "\nArea of a polygon with coordinates (-10,3) , (3,-2) , (6,2) , (6,3) , (4,5) = " << squaren(10, -10, 3, 3, -2, 6, 2, 6, 3, 4, 5);
+}
+
+
+
