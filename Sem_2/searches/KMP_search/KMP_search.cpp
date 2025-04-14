16ba137e2f8720570d78f41d3adc3528a1f71fd3
hiva <ilyashramchov05@gmail.com>
2025-04-14 10:17:42 +0500

    Add files via upload

---
+++ Sem_2/searches/KMP_search/KMP_search.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+int* calcPrefixFunc(string pat, int size)
+{
+	int* lps = new int[size]; 
+	int len = 0;
+	lps[0] = 0;
+	int i = 1;
+	while (i < size)
+	{
+		if (pat[i] == pat[len])
+		{
+			len++;
+			lps[i] = len;
+			i++;
+		}
+		else
+		{
+			if (len != 0)  len = lps[len - 1]; 
+			else  lps[i] = 0; i++; 
+		}
+	}
+	return lps;
+}
+
+
+void knuthMorrisPrattSearch(string str, string pat)
+{
+	int strSize = str.size();
+	int patSize = pat.size();
+	int* lps = calcPrefixFunc(pat, patSize);
+	int strIdx = 0;
+	int patIdx = 0;
+	while ((strSize - strIdx) >= (patSize - patIdx))
+	{
+		if (pat[patIdx] == str[strIdx])
+		{
+			patIdx++;
+			strIdx++;
+		}
+		if (patIdx == patSize)
+		{
+			cout << "Substring found from index: " << strIdx - patIdx << endl;
+			patIdx = lps[patIdx - 1];
+		}
+		else if (strIdx < strSize && pat[patIdx] != str[strIdx])
+		{
+			if (patIdx != 0) { patIdx = lps[patIdx - 1]; }
+			else { strIdx++; }
+		}
+	}
+}
+
+
+
+int main()
+{
+	string inputStr, searchStr;
+	cout << "Input original string " << '\n'; cin >> inputStr;
+	cout << "Input substring to find " << '\n'; cin >> searchStr;
+	knuthMorrisPrattSearch(inputStr, searchStr);
+	return 0;
+}
